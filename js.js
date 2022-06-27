3845b37e419a05560f52ae397cc168f7c3d2cc70
Rabonito <josephr.galicio@gmail.com>
2022-06-27 16:01:18 +0800

    v2.0 web portfolio updates and adds content

---
+++ js.js
@@ -0,0 +1,14 @@
+<<<<<<< HEAD
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+=======
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+>>>>>>> ef0785506e4e205d528d9016e3a7e2b953f332bf
+})
